52f11a9397e3f0b99ea1ea1e4f07112852895fde
Alex Eudes <alex.eudes@gmail.com>
2026-01-12 21:00:08 -0300

    Upgrading migrations with number of days logic

---
+++ Migrations/20260112235541_PassportNumberOfDays.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace pass_trip.Migrations
+{
+    public partial class PassportNumberOfDays : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "numberOfDays",
+                table: "Passports",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "numberOfDays",
+                table: "Passports");
+        }
+    }
+}
